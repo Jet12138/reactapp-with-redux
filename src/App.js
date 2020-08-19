1eaa581116ec80a51625494aa82d59d763c0908f
Jet12138 <390715001@qq.com>
2020-08-19 14:46:22 +0800

    学习redux , 然后是 react-redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+
+function Example() {
+    const [count, setCount] = useState(0);
+    
+    function handleAlertClick() {
+        setTimeout(() => {
+            alert('You clicked on: ' + count);
+        }, 1000);
+    }
+    
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+            <button onClick={handleAlertClick}>
+                Show alert
+            </button>
+        </div>
+    );
+}
+
 
 function App() {
   return (
@@ -50,6 +73,7 @@ function App() {
             React Redux
           </a>
         </span>
+        <Example/>
       </header>
     </div>
   );
---
+++ src/actionTypes/index.js
@@ -0,0 +1,7 @@
+/*
+ * action 类型
+ */
+export const ADD_TODO = 'ADD_TODO';  // 添加todo
+export const REMOVE_TODO = 'REMOVE_TODO';  // 去除todo
+export const TOGGLE_TODO = 'TOGGLE_TODO';  //切换todo状态
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';  //设置当前过滤todo的方式；
